603ad1ac006c6de8d451c324464996e63e81a754
Áxel Kovacki <axelkovacki7@gmail.com>
2021-02-24 14:11:41 -0300

    Remove json-crypto.

--- src/config/cryptography/Cryptography.ts
+++ src/config/cryptography/Cryptography.ts
@@ -1,17 +1,46 @@
-import { createCodec } from 'json-crypto';
+import Crypto from 'crypto';
 
 export default class Cryptography {
-    codec: any;
+    key: Buffer;
 
     constructor(secret: string) {
-        this.codec = createCodec(secret);
+        this.key = this.createKey(secret);
+    }
+
+    createKey(secret: string) {
+        return Crypto.createHash("sha256").update(secret).digest();
     }
 
     encrypt(payload: object) {
-        return this.codec.encrypt(payload);
+        const iv = Crypto
+            .createHash("sha256")
+            .update(Crypto.randomBytes(48).toString("hex"))
+            .digest();
+
+        const resizedIV = Buffer.allocUnsafe(16);
+        iv.copy(resizedIV);
+
+        const cipher = Crypto.createCipheriv("aes256", this.key, resizedIV);
+        
+        const msg = [];
+        msg.push(cipher.update(JSON.stringify(payload), "binary", "hex"));
+        msg.push(cipher.final("hex"));
+
+        return encodeURIComponent(
+            JSON.stringify([resizedIV.toString("hex"), msg.join("")])
+        );
     }
 
     decript(payload: string) {
-        return this.codec.decrypt(payload);
+        const [iv, ciphertext] = JSON.parse(decodeURIComponent(payload));
+
+        const bufferIV = Buffer.from(iv, "hex");
+        const decipher = Crypto.createDecipheriv("aes256", this.key, bufferIV);
+
+        const msg = [];
+        msg.push(decipher.update(ciphertext, "hex", "binary"));
+        msg.push(decipher.final("binary"));
+        
+        return JSON.parse(msg.join(""));
     }
 }
